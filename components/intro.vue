aee7197bef82b069aa1832e76a949e04ef940e04
clotairedamy <damynicholas@gmail.com>
2020-09-20 08:13:13 -0600

    first commit

---
+++ components/intro.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="animation1" ref="animone"></div>
+</template>
+<script>
+import lottie from "lottie-web/build/player/lottie";
+let lottieInstance = null;
+export default {
+  methods: {
+    playSegments() {
+      lottieInstance.playSegments(0, 1);
+    },
+  },
+  mounted() {
+    lottieInstance = lottie.loadAnimation({
+      container: this.$refs.animone,
+      renderer: "svg",
+      loop: false,
+      autoplay: false,
+      path: "intro.json",
+    });
+    this.playSegments();
+  },
+};
+</script>
+<style scoped>
+.animation1 {
+  background-color: #1d1d23;
+  min-height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
