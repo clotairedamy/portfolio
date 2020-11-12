c67d9229e3511345cc1461c61820ec866eee4915
ekopari <alexandrescuradumihai@gmail.com>
2020-11-12 16:02:19 +0200

    Multipart animation player 2

--- components/MultipartAnimationPlayer.vue
+++ components/MultipartAnimationPlayer.vue
@@ -1,54 +1,58 @@
 <template>
-    <LottieAnimationPlayer
+  <LottieAnimationPlayer
     @lottie-loaded="lottieLoaded"
     :animationFile="animationFile"
     :loopAnimation="false"
     :autoplay="false"
-    >
-    </LottieAnimationPlayer>
+  >
+  </LottieAnimationPlayer>
 </template>
 
 <script>
 export default {
-    /*
+  /*
     segment structure:
     {
         startFrame: number,
         endFrame: number,
-        autoAdvanceSegmentIndex: number
-    }
-    */ 
-    data() {
-        return {
-            lottieInstance: null,
-            currentSegment: 0,
-            cachedOperations: []
-        }
-    },
-    props: {
-        animationFile: { type: String, required: true },
-        fps: {type: Number, default: 30},
-        segments: {required: true},
-        timeSpeedRatio : {type: Number, default: 0.5}, // 0=constant time, 1=constant speed
-        maxTime: {type: Number, default: 5}, //seconds
-        maxSpeed: {type:Number, default: 5} //default speed multiplier
-    },
-    methods: {
-        lottieLoaded(lottieInstance) {
-            this.lottieInstance = lottieInstance;
-            for (const cachedOperation of this.cachedOperations) {
-                cachedOperation();
-            }
-        },
-        goToSegment(segmentNumber) {
-            if (this.lottieInstance === null) {
-                this.cachedOperations.push(() => {
-                    this.goToSegment(segmentNumber);
-                });
-                return;
-            }
+        autoAdvanceSegmentIndex: number|undefined,
+        autoplay: boolean,
 
-        }
+        // function is called by the player (this component)
+        // argument is supplied by the player (this component) and should be called to set the relative position (0-100%) of the current segment
+        registerController((positionInSegment: number) => {...}) 
     }
-}
+    */
+  data() {
+    return {
+      lottieInstance: null,
+      currentSegment: 0,
+      cachedOperations: [],
+    };
+  },
+  props: {
+    animationFile: { type: String, required: true },
+    fps: { type: Number, default: 30 },
+    segments: { required: true },
+    timeSpeedRatio: { type: Number, default: 0.5 }, // 0=constant time, 1=constant speed
+    maxTime: { type: Number, default: 5 }, //seconds
+    maxSpeed: { type: Number, default: 5 }, //default speed multiplier
+  },
+  methods: {
+    lottieLoaded(lottieInstance) {
+      this.lottieInstance = lottieInstance;
+      for (const cachedOperation of this.cachedOperations) {
+        cachedOperation();
+      }
+    },
+    goToSegment(segmentNumber) {
+      if (this.lottieInstance === null) {
+        this.cachedOperations.push(() => {
+          this.goToSegment(segmentNumber);
+        });
+        return;
+      }
+    },
+  },
+};
 </script>
