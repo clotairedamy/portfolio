82cf8fe3a9566faa85d6932e195c5230b51fe9fd
ekopari <alexandrescuradumihai@gmail.com>
2020-11-24 13:47:03 +0200

    BA update

--- components/BackgroundAnimation.vue
+++ components/BackgroundAnimation.vue
@@ -116,5 +116,11 @@ export default {
 <style>
 #map {
   position: fixed;
+  top: 50%;
+  left: 50%;
+  width: 100%;
+  height: 100%;
+  transform: translate(-50%, -50%);
+  will-change: transform;
 }
 </style>
