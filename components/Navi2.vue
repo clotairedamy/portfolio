97e726df810e5a400e723e91cbca1be7e3cdb07a
clotairedamy <damynicholas@gmail.com>
2020-11-25 09:45:00 -0700

    routingFix

--- components/Navi2.vue
+++ components/Navi2.vue
@@ -7,15 +7,20 @@
           class="flex justify-between items-center py-6 md:justify-start md:space-x-10"
         >
           <div class="flex justify-start lg:w-0 lg:flex-1">
-            <a href="#">
+            <a href="/">
               <span class="sr-only">Clotaire</span>
-              <img class="h-8 w-auto sm:h-10" src="clotaire-01.svg" alt="" />
+              <img
+                class="h-8 w-auto sm:h-10"
+                src="clotaire-01.svg"
+                alt="Clotaire Damy logo"
+              />
             </a>
           </div>
           <div class="-mr-2 -my-2 md:hidden">
             <button
               type="button"
-              class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500" @click="mobileMenuOpen=true"
+              class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500"
+              @click="mobileMenuOpen = true"
             >
               <span class="sr-only">Open menu</span>
               <!-- Heroicon name: menu -->
@@ -50,7 +55,7 @@
               Development
             </n-link>
             <n-link
-              to="Marketing"
+              to="marketing"
               class="text-base font-medium text-gray-500 hover:text-gray-900"
             >
               Marketing
@@ -80,7 +85,8 @@
         To: "opacity-0 scale-95"
     -->
       <div
-        class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden z-10" v-if="mobileMenuOpen"
+        class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden z-10"
+        v-if="mobileMenuOpen"
       >
         <div
           class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-gray-800 divide-y-2 divide-gray-50"
@@ -93,7 +99,8 @@
               <div class="-mr-2">
                 <button
                   type="button"
-                  class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500" @click="mobileMenuOpen=false"
+                  class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500"
+                  @click="mobileMenuOpen = false"
                 >
                   <span class="sr-only">Close menu</span>
                   <!-- Heroicon name: x -->
@@ -219,11 +226,11 @@
 
 <script>
 export default {
-    data() {
-        return {
-            mobileMenuOpen: false
-        }
-    }
+  data() {
+    return {
+      mobileMenuOpen: false
+    };
+  }
 };
 </script>
 
