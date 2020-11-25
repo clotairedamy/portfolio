5d8b6f2556ba63164ea3ee598b0345baa57658a3
clotairedamy <damynicholas@gmail.com>
2020-11-25 07:16:09 -0700

    working

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div>
     <BackgroundAnimation>
       <template v-slot:header>
-        <Navigation class="z-10 bg-transparent"/>
+        <Navigation class="z-10 bg-gray-900"/>
       </template>
       <Nuxt id="pageContent" />
       <template v-slot:footer>
