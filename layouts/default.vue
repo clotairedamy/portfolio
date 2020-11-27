3ad8e9cbc57034eca0b5da54f1126e60d3a527c4
clotairedamy <damynicholas@gmail.com>
2020-11-27 08:34:39 -0700

    fonts

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div>
     <BackgroundAnimation>
       <template v-slot:header>
-        <Navi2 class="z-10 bg-gray-900"/>
+        <Navi2 class="z-10 "/>
       </template>
       <Nuxt id="pageContent" />
       <template v-slot:footer>
