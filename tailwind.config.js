d7f6bc49f229065b5395895d45c84802b8517f7b
clotairedamy <damynicholas@gmail.com>
2020-11-27 11:07:45 -0700

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     fontFamily: {
       display: ["Track", "sans-serif"],
       body: ["bicyclette", "sans-serif"],
-      sans: ["adobe-handwriting-ernie"]
+      h4: ["adobe-handwriting-ernie", "sans-serif"]
     },
     extend: {
       colors: {
