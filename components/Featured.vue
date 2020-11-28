eadbcadb5e8e013059ee9790c9a79671e402f0df
clotairedamy <damynicholas@gmail.com>
2020-11-27 21:00:58 -0700

    content

--- components/Featured.vue
+++ components/Featured.vue
@@ -1,17 +1,14 @@
 <template>
   <!-- This example requires Tailwind CSS v2.0+ -->
-<div class="bg-gray-800">
-  <div class="max-w-7xl mx-auto py-16 px-6 sm:px-8 lg:py-24 lg:px-8 rounded shadow-lg mt-20 mb-20">
-    <div class="max-w-5xl mx-auto text-left">
+<div class="bg-transparent">
+  <div class="max-w-6xl mx-auto py-16 px-6 sm:px-8 lg:py-24 lg:px-8 rounded shadow-lg mt-20 mb-20">
+    <div class="max-w-6xl mx-auto text-left">
       <h2 class="text-3xl font-extrabold text-gray-600">Services</h2>
       <p class="mt-4 text-lg text-gray-500"> User Interfaces are driven by extensive research, motion and emotions. Motion design is giving users a sense of wonder, pleasure, the animation is acting as support for explaining complex benefits and features.</p>
     </div>
-    <dl class="mt-12 space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 lg:grid-cols-3 lg:gap-x-8">
+    <dl class="mt-12 space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 lg:grid-cols-3 lg:gap-x-4">
       <div class="flex">
-        <!-- Heroicon name: check -->
-        <svg class="flex-shrink-0 h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-        </svg>
+       
         <div class="ml-3">
           <dt class="text-lg leading-6 font-medium text-gray-600">
             UI Design
@@ -23,10 +20,7 @@
       </div>
 
       <div class="flex">
-        <!-- Heroicon name: check -->
-        <svg class="flex-shrink-0 h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-        </svg>
+      
         <div class="ml-3">
           <dt class="text-lg leading-6 font-medium text-gray-600">
             Web Development
@@ -38,10 +32,7 @@
       </div>
 
       <div class="flex">
-        <!-- Heroicon name: check -->
-        <svg class="flex-shrink-0 h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-        </svg>
+       
         <div class="ml-3">
           <dt class="text-lg leading-6 font-medium text-gray-600">
             Motion Design
@@ -52,50 +43,10 @@
         </div>
       </div>
 
-      <div class="flex">
-        <!-- Heroicon name: check -->
-        <svg class="flex-shrink-0 h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-        </svg>
-        <div class="ml-3">
-          <dt class="text-lg leading-6 font-medium text-gray-600">
-            Search Engine Optimization
-          </dt>
-          <dd class="mt-2 text-base text-gray-500">
-            Interdum quam pulvinar turpis tortor, egestas quis diam amet, natoque. Mauris sagittis.
-          </dd>
-        </div>
-      </div>
 
-      <div class="flex">
-        <!-- Heroicon name: check -->
-        <svg class="flex-shrink-0 h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-        </svg>
-        <div class="ml-3">
-          <dt class="text-lg leading-6 font-medium text-gray-600">
-            Technical SEO
-          </dt>
-          <dd class="mt-2 text-base text-gray-500">
-            Ullamcorper in ipsum ac feugiat. Senectus at aliquam vulputate mollis nec. In at risus odio.
-          </dd>
-        </div>
-      </div>
+      
 
-      <div class="flex">
-        <!-- Heroicon name: check -->
-        <svg class="flex-shrink-0 h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-        </svg>
-        <div class="ml-3">
-          <dt class="text-lg leading-6 font-medium text-gray-600">
-            Conversion Optimization
-          </dt>
-          <dd class="mt-2 text-base text-gray-500">
-            Magna a vel sagittis aliquam eu amet. Et lorem auctor quam nunc odio. Sed bibendum.
-          </dd>
-        </div>
-      </div>
+     
 
       
 
--- components/Header.vue
+++ components/Header.vue
@@ -1,34 +1,71 @@
 <template>
   <div
-    class="mx-auto max-w-screen-6xl px-6 sm:px-6 md:mt-16 mt-10 lg:mt-20 sm:mt-10 z-20 opacity-30 overscroll-auto"
+    class="mx-auto max-w-screen-6xl px-6 sm:px-6 md:mt-20 mb-10 lg:mt-20 sm:mt-10 z-20 opacity-30 "
   >
-    <div class="relative h-auto">
-      <div class="absolute inset-0 flex flex-col" aria-hidden="true">
-        <div class="flex-1 "></div>
-        <div class="flex-1"></div>
-      </div>
+    <div class="bg-transparent">
       <div
-        class="relative max-w-6xl  mx-auto h-auto px-2 sm:px-2 lg:px-2"
+        class="max-w-6xl mx-auto py-16 px-6 sm:px-8 lg:py-24 lg:px-8 rounded shadow-lg mt-20 mb-20"
       >
-        <div
-          class="md:bg-gradient-to-r from-gray-900 to-transparent rounded-lg shadow-xl overflow-hidden lg:grid lg:grid-cols-2 lg:gap-4 mt-auto"
+        <div class="max-w-6xl mx-auto text-left">
+          <h2 class="text-3xl font-extrabold text-gray-600 mb-20">
+            {{ headline }}<span class="text-gray-300">{{ headlineTwo }}</span>
+          </h2>
+          <p class="mt-4 text-lg text-gray-500">{{ content }}</p>
+        </div>
+        <dl
+          class="mt-12 space-y-10 sm:space-y-0 sm:grid sm:grid-cols-3 sm:gap-x-6 sm:gap-y-12 lg:grid-cols-3 lg:gap-x-4"
         >
-          <div
-            class=" pb-12 px-6 sm:pt-10 sm:px-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20"
-          >
-            <div class="md:self-center">
-              <h2 class="text-9xl font-extrabold text-gray-300 sm:text-4xl mb-20">
-                <span >{{ headline }}</span>
-                <span class=" text-gray-600">{{ headlineTwo }}</span>
-              </h2>
-              <p class="mt-4 text-md leading-6 text-gray-200">{{ content }}</p>
+          <div class="flex">
+           
+            <div class="ml-3 ">
+              <dt class="text-lg leading-6 font-medium text-gray-600">
+                {{ serviceOneTitle }}
+              </dt>
+              <dd class="mt-2 text-base text-gray-500">
+                {{ serviceOneDescription }}
+              </dd>
             </div>
           </div>
+
+          <div class="flex">
+         
+            <div class="ml-3">
+              <dt class="text-lg leading-6 font-medium text-gray-600">
+                {{ serviceTwoTitle }}
+              </dt>
+              <dd class="mt-2 text-base text-gray-500">
+                {{ serviceTwoDescription }}
+              </dd>
+            </div>
+          </div>
+
+          <div class="flex">
+         
+            <div class="ml-3">
+              <dt class="text-lg leading-6 font-medium text-gray-600">
+                {{ serviceThreeTitle }}
+              </dt>
+              <dd class="mt-2 text-base text-gray-500">
+                {{ serviceThreeDescription }}
+              </dd>
+            </div>
+          </div>
+        </dl>
+      </div>
+    </div>
+    <div class="relative">
+      <div class="absolute inset-0 flex flex-col" aria-hidden="true">
+        <div class="flex-1"></div>
+      </div>
+      <div class="relative max-w-6xl  mx-auto  px-2 sm:px-2 lg:px-2">
+        <div
+          class="bg-transparent rounded-lg  overflow-hidden lg:grid lg:grid-cols-2 lg:gap-4 mt-auto"
+        >
           <div
-            class="mt-auto pb-12 px-6 font-bold sm:pt-16 sm:px-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20"
+            class="mt-auto pb-2 px-6 font-bold sm:pt-16 sm:px-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20"
           >
             <div class="lg:self-center">
-              <h4 class="text-2xl special text-pink-600 sm:text-4xl">
+              <h4 class="text-sm special  sm:text-2xl">
                 <span class="block text-gray-400">{{ quote }}</span>
               </h4>
               <p class="mt-4 text-lg leading-6 text-gray-600">{{ name }}</p>
@@ -47,13 +84,18 @@ export default {
     headlineTwo: { type: String, required: true },
     content: { type: String, required: true },
     quote: { type: String, default: true },
-    name: { type: String, default: true }
+    name: { type: String, default: true },
+    serviceOneTitle: { type: String, default: "Title" },
+    serviceOneDescription: { type: String, default: "Description" },
+    serviceTwoTitle: { type: String, default: "Title" },
+    serviceTwoDescription: { type: String, default: "Description" },
+    serviceThreeTitle: { type: String, default: "Title" },
+    serviceThreeDescription: { type: String, default: "Description" }
   }
 };
 </script>
 
 <style>
-
 .home-enter-active,
 .home-leave-active {
   transition: slide-in 0.5s;
@@ -68,7 +110,7 @@ h1 {
 }
 .special {
   font-family: "Autography";
-  
+
   line-height: 1;
 }
 </style>
