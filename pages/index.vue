1b450a2101ca8019410af85fa9efb30cd03b53c1
ekopari <alexandrescuradumihai@gmail.com>
2020-12-01 18:00:47 +0200

    index +1

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,21 @@
+<template>
+    <div class="py-12 bg-transparent">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <Title
+        title="Clotaire Damy"
+        content="A Developer and Digital Marketer based in Colorado Springs (US). I’m passionate about transforming complex problems into meaningful and engaging web interfaces."
+        subtitle="Front-end Development"
+      />
+      <h3>Alte lucruri...</h3>
+      <!--citatul intra aici-->
+    </div>
+  </div>
+</template>
 <script>
 export default {
-    middleware({redirect}) {
-        return redirect("/about");
-    }
+
 }
 </script>
+<style scoped>
+
+</style>
