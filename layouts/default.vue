f64cff37aa3f9cecf1f9addc169d1f8f758c2102
clotairedamy <damynicholas@gmail.com>
2020-12-01 12:06:13 -0700

    scrollbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div>
     <BackgroundAnimation>
       <template>
-        <Navigation class="z-10 "/>
+        <Navigation class="z-10 " />
       </template>
       <Nuxt id="pageContent" />
       <template v-slot:footer>
@@ -16,6 +16,19 @@
 body {
   background-color: #111827;
   overflow-x: hidden;
+ 
+}
+body::-webkit-scrollbar {
+  width: 1em;
+}
+ 
+body::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 6px #F59E0B;
+}
+ 
+body::-webkit-scrollbar-thumb {
+  background-color: rgb(34, 31, 31);
+  outline: 20px solid #F59E0B;
 }
 #pageContent {
   position: relative;
@@ -23,5 +36,3 @@ body {
   background-color: transparent;
 }
 </style>
-
-
