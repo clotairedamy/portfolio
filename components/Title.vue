b029b5c6d26f592f42e4ce0a9df7c745304ad8ec
clotairedamy <damynicholas@gmail.com>
2020-12-03 08:09:17 -0700

    titleEffect

--- components/Title.vue
+++ components/Title.vue
@@ -5,12 +5,12 @@
 
     <div class="max-w-7xl mx-auto  sm:px-6 lg:px-8">
       <div class="title">
-        <h1 class="text-5xl font-extrabold ">{{ title }}</h1>
-          <h2 class="text-3xl font-extrabold ">{{ subtitle }}</h2>
+        <h3 class="text-xl text-gray-500 font-extrabold tracking-tighter">{{ title }}</h3>
+          <h2 class="text-6xl font-black tracking-widest">{{ subtitle }}</h2>
       </div>
       <div class="lg:text-left">
         <p
-          class="mt-2 text-xl leading-8  text-gray-500 sm:text-2xl"
+          class="mt-20 text-sm leading-8  text-gray-500 sm:text-2xl"
         >
           {{ content }}
         </p>
@@ -46,13 +46,13 @@ export default {
 
 .title {
 	
-	color: #FFF;
+	
 	display: flex;
 	flex-direction: column;
 	align-items: center;
 	justify-content: center;
-	height: 30vh;
-	letter-spacing: 10px;  font-family: "Track";
+	height: 15vh;
+	font-family: "Track";
  
 }
 
@@ -64,8 +64,8 @@ h1,h2 {
 	-moz-background-clip: text;
 	-webkit-background-clip: text;
 	text-transform: uppercase;
-	line-height: 1.5;
-	margin: 10px 0;
+	line-height: 1.2;
+
 }
   
 
--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,9 @@
   <div class="py-4 bg-transparent">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-10 sm:mt-4">
       <Title
-        title="Clotaire"
+        title="Damy"
         content="Based in Colorado Springs, passionate about transforming complex problems into meaningful and engaging web interfaces."
-        subtitle="Damy"
+        subtitle="Clotaire"
       />
      
     </div>
